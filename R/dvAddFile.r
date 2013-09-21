f43382bb4c6ccc508460edc8e0c9a111fb7fcd8b
leeper <thosjleeper@gmail.com>
2013-09-21 16:42:08 +0200

    edit file read in dvAddFile

--- R/dvAddFile.r
+++ R/dvAddFile.r
@@ -1,8 +1,8 @@
-dvAddFile <- function(user, pwd, objectid, xmlfile, dv=getOption('dvn'), browser=FALSE, ...){
+dvAddFile <- function(user, pwd, objectid, file, dv=getOption('dvn'), browser=FALSE, ...){
     # need to be able to handle xml character string and xml file path
     xml <- dvDepositQuery(  query=paste('edit-media/study/',objectid,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
-                            httpverb='POST', postfields=charToRaw(paste(readLines(xmlfile),collapse="")),
-                            httpheader=c(   'Content-Disposition'='filename=example.zip',
+                            httpverb='POST', postfields=readBin(file,what='raw'),
+                            httpheader=c(   'Content-Disposition'=paste("filename",file,sep="="),
                                             'Content-Type'='application/atom+xml',
                                             'Packaging'='http://purl.org/net/sword/package/SimpleZip'))
     if(is.null(xml))
