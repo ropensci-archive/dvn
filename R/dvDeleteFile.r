f83c5c05415d0927d4f0cfb66542fe863e0a8cc8
leeper <thosjleeper@gmail.com>
2013-09-22 10:58:02 +0200

    fixed dvDeleteFile.r

--- R/dvDeleteFile.r
+++ R/dvDeleteFile.r
@@ -1,6 +1,6 @@
-dvDeleteFile <- function(user, pwd, fileid, xmlfile, dv=getOption('dvn'), browser=FALSE, ...){
+dvDeleteFile <- function(user, pwd, fileid, dv=getOption('dvn'), browser=FALSE, ...){
     # need to be able to handle xml character string and xml file path
-    xml <- dvDepositQuery(  query=paste('edit-media/study/',objectid,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
+    xml <- dvDepositQuery(  query=paste('edit-media/file/',fileid,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
                             httpverb='DELETE')
     if(is.null(xml))
 		invisible(NULL)
