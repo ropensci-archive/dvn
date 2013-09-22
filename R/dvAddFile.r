8d183e918d6e224948684115a364d409c029f34b
leeper <thosjleeper@gmail.com>
2013-09-22 11:29:51 +0200

    still working on dvAddFile and dvEdityStudy problems

--- R/dvAddFile.r
+++ R/dvAddFile.r
@@ -1,9 +1,8 @@
-dvAddFile <- function(user, pwd, objectid, file, dv=getOption('dvn'), browser=FALSE, ...){
-    # need to be able to handle xml character string and xml file path
+dvAddFile <- function(user, pwd, objectid, filename, dv=getOption('dvn'), browser=FALSE, ...){
     xml <- dvDepositQuery(  query=paste('edit-media/study/',objectid,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
-                            httpverb='POST', postfields=readBin(file,what='raw'),
-                            httpheader=c(   'Content-Disposition'=paste("filename",file,sep="="),
-                                            'Content-Type'='application/atom+xml',
+                            httpverb='POST', postfields=filename, upload=TRUE,
+                            httpheader=c(   'Content-Disposition'=paste('filename',filename,sep='='),
+                                            'Content-Type'='application/zip',
                                             'Packaging'='http://purl.org/net/sword/package/SimpleZip'))
     if(is.null(xml))
 		invisible(NULL)
--- R/dvDepositQuery.r
+++ R/dvDepositQuery.r
@@ -12,10 +12,10 @@ dvDepositQuery <- function(query, user, pwd, dv=getOption('dvn'), browser=FALSE,
                 dv <- paste(dv,"/",sep="")
         }
         url <- paste(dv,"api/data-deposit/",apiversion,"/swordv2/",query,sep="")
-        userpwd <- paste(user,pwd,sep=':')
     }
     else
         url <- fulluri
+    userpwd <- paste(user,pwd,sep=':')
     if(browser==TRUE & httpverb=='GET'){
         tmp <- strsplit(url,"://")[[1]]
         browseURL(paste(tmp[1],'://',userpwd,'@',tmp[2],sep=''))
@@ -24,36 +24,37 @@ dvDepositQuery <- function(query, user, pwd, dv=getOption('dvn'), browser=FALSE,
         stop('If httpverb != GET, browser must be FALSE')
     else if(httpverb=='GET'){
         xml <- getURL(url, followlocation = TRUE, userpwd=userpwd,
-                    #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
                     ssl.verifypeer = FALSE, ssl.verifyhost = FALSE, ...)
+                    #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
                     #cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
         return(xml)
     }
     else if(httpverb=='POST'){
         # POST to handle dvCreateStudy and dvAddFile
         h <- basicTextGatherer()
-        xml <- curlPerform(url = url, followlocation = TRUE, userpwd=userpwd,
-                    #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
+        xml <- curlPerform(url = url, userpwd=userpwd, followlocation = TRUE, customrequest = 'POST',
                     ssl.verifypeer = FALSE, ssl.verifyhost = FALSE, writefunction = h$update, verbose=TRUE, ...)
+                    #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
                     #cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
-        return(list(xml,h$value()))
+        return(h$value())
     }
     else if(httpverb=='PUT'){
         # PUT to handle dvEditStudy
         h <- basicTextGatherer()
-        xml <- curlPerform(url = url, followlocation = TRUE, userpwd=userpwd,
-                    #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
+        xml <- curlPerform(url = url, userpwd=userpwd, followlocation = TRUE, customrequest = 'PUT', 
                     ssl.verifypeer = FALSE, ssl.verifyhost = FALSE, writefunction = h$update,
-                    verbose=TRUE, upload=TRUE, ...)
+                    verbose=TRUE, ...)
+                    #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
                     #cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
         return(h$value())
     }
     else if(httpverb=='DELETE'){
         # DELETE to handle dvDeleteStudy and dvDeleteFile
         h <- basicTextGatherer()
-        xml <- curlPerform(url = url, followlocation = TRUE, userpwd=userpwd, customrequest = 'DELETE',
+        xml <- curlPerform(url = url, userpwd=userpwd, followlocation = TRUE, customrequest = 'DELETE',
+                    ssl.verifypeer = FALSE, ssl.verifyhost = FALSE,
+                    writefunction = h$update, verbose=TRUE, ...)
                     #ssl.verifypeer = TRUE, ssl.verifyhost = TRUE,
-                    ssl.verifypeer = FALSE, ssl.verifyhost = FALSE, writefunction = h$update, verbose=TRUE, ...)
                     #cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
         return(h$value())
     }
