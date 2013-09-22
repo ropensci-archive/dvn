852cf7d2631308289a99ba8ec42ef1dc871e6653
leeper <thosjleeper@gmail.com>
2013-09-22 09:51:43 +0200

    Add support for using full URI in dvDepositQuery

--- R/dvDepositQuery.r
+++ R/dvDepositQuery.r
@@ -1,17 +1,21 @@
-dvDepositQuery <- function(query, user, pwd, dv=getOption('dvn'), browser=FALSE, apiversion='v1', httpverb='GET', ...){
+dvDepositQuery <- function(query, user, pwd, dv=getOption('dvn'), browser=FALSE, apiversion='v1', httpverb='GET', fulluri=NULL, ...){
     # Data Deposit API workhorse
-    if(is.null(dv) || dv=="")
-        stop("Must specify Dataverse URL as 'dv'")
-    else{
-        https <- substring(dv,1,5)
-        if(!https=="https")
-            stop("API query must use https")
-        slash <- substring(dv,nchar(dv),nchar(dv))
-        if(!slash=="/")
-            dv <- paste(dv,"/",sep="")
+    if(is.null(fulluri)){
+        if(is.null(dv) || dv=="")
+            stop("Must specify Dataverse URL as 'dv'")
+        else{
+            https <- substring(dv,1,5)
+            if(!https=="https")
+                stop("API query must use https")
+            slash <- substring(dv,nchar(dv),nchar(dv))
+            if(!slash=="/")
+                dv <- paste(dv,"/",sep="")
+        }
+        url <- paste(dv,"api/data-deposit/",apiversion,"/swordv2/",query,sep="")
+        userpwd <- paste(user,pwd,sep=':')
     }
-    url <- paste(dv,"api/data-deposit/",apiversion,"/swordv2/",query,sep="")
-    userpwd <- paste(user,pwd,sep=':')
+    else
+        url <- fulluri
     if(browser==TRUE & httpverb=='GET'){
         tmp <- strsplit(url,"://")[[1]]
         browseURL(paste(tmp[1],'://',userpwd,'@',tmp[2],sep=''))
