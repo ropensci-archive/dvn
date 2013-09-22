d7d700907209e84ce74e7a386fc8ed4915bd02be
leeper <thosjleeper@gmail.com>
2013-09-22 11:09:42 +0200

    resolved dvEditStudy problems

--- R/dvEditStudy.r
+++ R/dvEditStudy.r
@@ -1,7 +1,8 @@
 dvEditStudy <- function(user, pwd, objectid, xmlfile, dv=getOption('dvn'), browser=FALSE, ...){
     # need to be able to handle xml character string and xml file path
+    filetosend <- charToRaw(paste(readLines(xmlfile),collapse=""))
     xml <- dvDepositQuery(  query=paste('edit/study/',objectid,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
-                            httpverb='PUT', postfields=charToRaw(paste(readLines(xmlfile),collapse="")),
+                            httpverb='PUT', postfields=filetosend,
                             httpheader=c('Content-Type'='application/atom+xml'))
     if(is.null(xml))
 		invisible(NULL)
