3727068c9af2b6dae79e04f91d69b9c532973719
leeper <thosjleeper@gmail.com>
2013-09-24 17:38:18 +0200

    allow xml to be character or file path for v0.2.45

--- R/dvCreateStudy.r
+++ R/dvCreateStudy.r
@@ -1,11 +1,16 @@
 dvCreateStudy <-
 function(   dvname, xmlfile, dv=getOption('dvn'),
             user=getOption('dvn.user'), pwd=getOption('dvn.pwd'), browser=FALSE, ...){
-    # need to be able to handle xml character string and xml file path
     if(is.null(user) | is.null(pwd))
         stop('Must specify username (`user`) and password (`pwd`)')
+    if(is.null(xmlfile) || !is.character(xmlfile))
+        stop('`xmlfile` must be xml character string or path to xml file')
+    if(file.exists(xmlfile))
+        filetosend <- charToRaw(paste(readLines(xmlfile),collapse=""))
+    else
+        filetosend <- charToRaw(xmlfile)
     xml <- dvDepositQuery(  query=paste('collection/dataverse/',dvname,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
-                            httpverb='POST', postfields=charToRaw(paste(readLines(xmlfile),collapse="")),
+                            httpverb='POST', postfields=filetosend,
                             httpheader=c('Content-Type'='application/atom+xml'))
     if(is.null(xml))
 		invisible(NULL)
--- R/dvEditStudy.r
+++ R/dvEditStudy.r
@@ -1,10 +1,14 @@
 dvEditStudy <-
 function(   objectid, xmlfile, dv=getOption('dvn'),
             user=getOption('dvn.user'), pwd=getOption('dvn.pwd'), browser=FALSE, ...){
-    # need to be able to handle xml character string and xml file path
     if(is.null(user) | is.null(pwd))
         stop('Must specify username (`user`) and password (`pwd`)')
-    filetosend <- charToRaw(paste(readLines(xmlfile),collapse=""))
+    if(is.null(xmlfile) || !is.character(xmlfile))
+        stop('`xmlfile` must be xml character string or path to xml file')
+    if(file.exists(xmlfile))
+        filetosend <- charToRaw(paste(readLines(xmlfile),collapse=""))
+    else
+        filetosend <- charToRaw(xmlfile)
     xml <- dvDepositQuery(  query=paste('edit/study/',objectid,sep=''), user=user, pwd=pwd, dv=dv, browser=browser,
                             httpverb='PUT', postfields=filetosend,
                             httpheader=c('Content-Type'='application/atom+xml'))
