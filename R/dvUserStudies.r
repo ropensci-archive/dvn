2500d25bef433fa5ad82ceccadba5725b28b3535
leeper <thosjleeper@gmail.com>
2013-10-10 10:24:18 +0200

    change dvUserStudies return value (closes #9)

--- R/dvUserStudies.r
+++ R/dvUserStudies.r
@@ -6,6 +6,17 @@ function(   dvname, dv=getOption('dvn'), user=getOption('dvn.user'),
     xml <- dvDepositQuery(query=paste('collection/dataverse/',dvname,sep=''), user=user, pwd=pwd, dv=dv, browser=browser, ...)
     if(is.null(xml))
 		invisible(NULL)
-	if(browser==FALSE)
-		return(xml)
+	if(browser==FALSE){
+		z <- xmlToList(xml)
+        tmp <- list()
+        tmp$dvtitle <- z$title$text
+        tmp$released <- z$dataverseHasBeenReleased
+        tmp$generator <- z$generator
+        tmp$studies <- do.call(rbind,lapply(z[names(z)=='entry'], function(i) c(title=i$title$text, objectId=i$id)))
+        rownames(tmp$studies) <- seq(rownames(tmp$studies))
+        tmp$studies <- as.data.frame(tmp$studies, stringsAsFactors=FALSE)
+        tmp$studies$objectId <- sapply(tmp$studies$objectId,function(i) strsplit(i,'study/')[[1]][2])
+        tmp$xml <- xml
+        return(tmp)
+    }
 }
