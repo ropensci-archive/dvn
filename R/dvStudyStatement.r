6a874051a522d1f000ee61c587d43315a294e088
leeper <thosjleeper@gmail.com>
2013-10-10 10:56:31 +0200

    fix output

--- R/dvStudyStatement.r
+++ R/dvStudyStatement.r
@@ -22,7 +22,7 @@ function(   objectid, dv=getOption('dvn'), user=getOption('dvn.user'),
         rownames(resources) <- seq(1,nrow(resources))
         rm(tmp)
         xmlout$files <- as.data.frame(resources, stringsAsFactors=FALSE)
-        xmlout$files$fileId <- sapply(z$files$src, function(i) strsplit(strsplit(i,'file/')[[1]][2],'/')[[1]][1])
+        xmlout$files$fileId <- sapply(xmlout$files$src, function(i) strsplit(strsplit(i,'file/')[[1]][2],'/')[[1]][1])
         xmlout$xml <- xml
 		return(xmlout)
     }
