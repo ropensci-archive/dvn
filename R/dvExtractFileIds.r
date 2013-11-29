c1a08ebc7c3da441e0eaa15a20bf140fc5214ed4
Thomas J. Leeper <thosjleeper@gmail.com>
2013-11-29 19:58:07 +0100

    extract non-data files from metadata (closes #16)

--- R/dvExtractFileIds.r
+++ R/dvExtractFileIds.r
@@ -2,18 +2,41 @@ dvExtractFileIds <- function(xml){
 	if(is.null(xmlChildren(xmlParse(xml))$codeBook))
         stop("Metadata format not currently supported. Must be 'ddi'.")
     nodes <- xmlChildren(xmlChildren(xmlParse(xml))$codeBook)
-	dscrs <- nodes[names(nodes)=="fileDscr"]
-	d <- data.frame(matrix(nrow=length(dscrs),ncol=5))
-	names(d) <- c("fileName","fileId","URI","caseQnty","varQnty")
-	for(i in 1:length(dscrs)){
-		attrs <- xmlAttrs(dscrs[[i]])
-		d$fileName[i] <- xmlValue(xmlChildren(xmlChildren(dscrs[[i]])$fileTxt)$fileName)
-		d$fileId[i] <- attrs[names(attrs)=="ID"]
-		d$fileId[i] <- substring(d$fileId[i],2,nchar(d$fileId[i])) # remove leading 'f' from fileId
-		d$URI[i] <- attrs[names(attrs)=="URI"]
-		dims <- xmlChildren(xmlChildren(xmlChildren(dscrs[[i]])$fileTxt)$dimensns)
-		d$caseQnty[i] <- xmlValue(dims$caseQnty)
-		d$varQnty[i] <- xmlValue(dims$varQnty)
-	}
-	return(d)
+	
+    # data files
+    dscrs <- nodes[names(nodes)=="fileDscr"]
+	d <- 
+    lapply(dscrs, function(i){
+        attrs <- xmlAttrs(i)
+        fileName <- xmlValue(xmlChildren(xmlChildren(i)$fileTxt)$fileName)
+        fileId <- attrs[names(attrs)=="ID"]
+		fileId <- substring(fileId,2,nchar(fileId)) # remove leading 'f' from fileId
+		URI <- attrs[names(attrs)=="URI"]
+		dims <- xmlChildren(xmlChildren(xmlChildren(i)$fileTxt)$dimensns)
+		caseQnty <- xmlValue(dims$caseQnty)
+		varQnty <- xmlValue(dims$varQnty)
+        return(list(fileName=fileName,
+                    fileId=fileId,
+                    URI=URI,
+                    caseQnty=caseQnty,
+                    varQnty=varQnty))
+    })
+    d <- do.call(rbind.data.frame, d)
+    rownames(d) <- NULL
+    
+    # other files
+    other <- nodes[names(nodes)=="otherMat"]
+    e <- 
+    lapply(other, function(i){
+        URI <- xmlAttrs(i)['URI']
+        fileName <- xmlValue(xmlChildren(i)$labl)
+        fileId <- strsplit(URI, 'fileId=')[[1]][2]
+        return(list(fileName = fileName,
+                    fileId = fileId,
+                    URI = URI))
+    })
+    e <- do.call(rbind.data.frame, e)
+    rownames(e) <- NULL
+    
+    return(merge(d, e, all=TRUE))
 }
