6c05c9c49a14ea814c1d43702c287b9f2306c15e
Thomas J. Leeper <thosjleeper@gmail.com>
2013-12-06 16:01:20 +0100

    playing around with dvAddFile

--- R/dvAddFile.r
+++ R/dvAddFile.r
@@ -23,7 +23,7 @@ function(   objectid, filename=NULL, dataframe=NULL, category=NULL, dv=getOption
     if(!is.null(filename))
         filename <- file.copy(from=filename, to=catdir, overwrite=TRUE)
     filename <- c(filename, dataframe)
-    if(length(filename)>1 || (length(filename)==1 && !tools::file_ext(filename)=='zip')){
+    if(length(filename)>1 | (length(filename)==1 && !tools::file_ext(filename)=='zip')){
         d <- getwd()
         setwd(tempdir())
         tmpzip <- tempfile(fileext='.zip')
@@ -34,18 +34,25 @@ function(   objectid, filename=NULL, dataframe=NULL, category=NULL, dv=getOption
                         list.files(paste('.',category,sep='/'))))
         setwd(d)
     }
+    #file <- file(tmpzip, 'rb', raw=TRUE)
     xml <- dvDepositQuery(  query=paste('edit-media/study/',objectid,sep=''),
             user=user, pwd=pwd, dv=dv, browser=browser,
-            httpverb='POST', #upload=TRUE,
-            #postfields=readBin(tmpzip,what='raw',file.info(tmpzip)$size),
-            postfields=readBin(tmpzip,what='raw'),
+            httpverb='POST',
+            #upload=TRUE, 
+            #readfunction = RCurl:::uploadFunctionHandler(file, FALSE),
+            #readfunction = readBin(file,what='raw',1e6),
+            unrestricted.auth = 1L,
+            postfields=readBin(tmpzip,what='raw',1e6),
+            postfieldsize=file.info(tmpzip)$size,
+            #postfields=readBin(tmpzip,what='raw',1e6),
             httpheader=c(   'Content-Disposition'=paste('attachment; filename',basename(tmpzip),sep='='),
                             'Content-Type'='application/zip',
                             'Packaging'='http://purl.org/net/sword/package/SimpleZip'))
     file.remove(file.path(catdir,list.files(catdir)))
-    file.remove(tmpzip)
+    #file.remove(tmpzip)
+    #close(file)
     if(is.null(xml))
-		return(NULL)
+        return(NULL)
 	if(browser==FALSE)
-		.dvParseAtom(xml)
+        .dvParseAtom(xml)
 }
